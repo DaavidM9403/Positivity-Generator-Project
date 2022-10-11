7f30a73bb057a444cd0e3567239b8600674cadee
DaavidM <113436157+DaavidM9403@users.noreply.github.com>
2022-10-11 11:12:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Secure Code")
+
+username = input("Username >")
+password = input("PWD >")
+
+if username == "mark" and password == "dubnation":
+  print("Welcome Mark!")
+elif username == "David" and password == "GODdid" :
+  print("BlessUp")
+else:
+  print("Try again")
